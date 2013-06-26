ec6293451f839d5c6bc160880a1d5c96c291d638
Winter <qq125594648@gmail.com>
2013-06-26 17:05:22 +0900

    Test changes kobuki_asmd/src/tools/Map.cpp

--- kobuki_asmd/src/tools/Map.cpp
+++ kobuki_asmd/src/tools/Map.cpp
@@ -24,14 +24,17 @@ class Map {
 public:
   Map(){
     this->max.setCoordinate(0,0);
+    this->current_block = NULL;
     //this->KobukiManager[0] = KobukiManager();
     //this->KobukiManager[1] = KobukiManager();
   }
   Map(int coord_x, int coord_y){
     this->max.setCoordinate(coord_x, coord_y);
+    this->current_block = NULL;
   }
   void resize(Coordinate coord){
     this->max = coord;
+    this->current_block = NULL;
   }
 
   bool blockListInit(){
@@ -39,35 +42,43 @@ public:
     Coordinate coord;
     int idx_x = this->max.getCoordinateX()/DEFAULT_BLOCK_LENGTH;
     int idx_y = this->max.getCoordinateY()/DEFAULT_BLOCK_LENGTH;
-    //std::cout << "idx_x = " << idx_x << std::endl; 
-    //std::cout << "idx_y = " << idx_y << std::endl;
-    for(int i = 0; i<idx_x; i++) {
+    std::cout << "idx_x = " << idx_x << std::endl; 
+    std::cout << "idx_y = " << idx_y << std::endl;
+   
+
+    if( idx_x>0 && idx_y>0 ){
+      for(int i = 0; i<idx_x; i++) {
       
-      std::vector<Block> list;
+        std::vector<Block> list;
       
-      for(int j = 0; j<idx_y; j++) {
-        coord.setCoordinate(20 + i*DEFAULT_BLOCK_LENGTH, 20 + j*DEFAULT_BLOCK_LENGTH);
-        Block block(coord, DEFAULT_BLOCK_LENGTH, i, j);
-	list.push_back(block);
+        for(int j = 0; j<idx_y; j++) {
+          coord.setCoordinate(20 + i*DEFAULT_BLOCK_LENGTH, 20 + j*DEFAULT_BLOCK_LENGTH);
+          Block block(coord, DEFAULT_BLOCK_LENGTH, i, j);
+  	  list.push_back(block);
+        }
+
+        this->block_list.push_back(list);   
+        list.clear(); 
       }
-
-      this->block_list.push_back(list);   
-      list.clear(); 
-    }
-
-    if(!block_list.empty()){
       this->current_block = (Block*) &block_list[0][0];
       block_list[0][0].setMark(BLANK);
       block_list[0][0].setHasKobuki(true);
       std::cout << "Block list initialezed successfully." << std::endl;
+      std::cout << "Size of block_list is [" << this->block_list.size() <<"]" << std::endl;
       return true;
     } else {
-      std::cout << "Block list initialeze failed." << idx_x << std::endl;
+      this->current_block = NULL;
+      std::cout << "Block list initialeze failed." << std::endl;
       return false;
     }
   }
   
-  Block* getNextTarget(){
+  Block* getNextBlock(){
+    
+    if(this->block_list.empty()){
+      return NULL;
+    }
+
     int now_x = this->current_block->getTagX();
     int now_y = this->current_block->getTagY();
     
@@ -89,17 +100,22 @@ public:
 
   void getMapInfo(){
 
-    std::cout << "This map has a max coordinate of [" << this->max.getCoordinateX() << ", " << this->max.getCoordinateY() << "]" << std::endl;
+    std::cout << "This map has a max coordinate of [" << this->max.getCoordinateX() << "," << this->max.getCoordinateY() << "]" << std::endl;
     if(!block_list.empty()){
       std::cout << "This map has a max block x of [" << this->block_list.size() << "]." << std::endl;
       std::cout << "This map has a max block y of [" << this->block_list.begin()->size() << "]." << std::endl;
+    } else {
+      std::cout << "This map has no block." << std::endl;
     }
   }
   
   Block* getCurrentBlock(){
     return this->current_block;
   }
-  
+  void setCurrentBlock(Block* b){
+    this->current_block = b;
+    return;
+  }
 private:
   Coordinate max;
   std::vector< std::vector<Block> > block_list;
@@ -111,13 +127,18 @@ private:
 ** Test Main
 *****************************************************************************/
 
-int main() {
-    Map m(150,150);
+int main(int argc, char* argv[]) {
+
+    Map m(120,200);
     m.blockListInit();
     m.getMapInfo();
-    Block* b = m.getNextTarget();
-    std::cout<<"Kobuki is now at block ["<< m.getCurrentBlock()->getTagX() <<", "<< m.getCurrentBlock()->getTagY() <<"]."<<std::endl;
-    std::cout<<"Next block is ["<< b->getTagX() <<", "<< b->getTagY() <<"]."<<std::endl;
+    Block* b = m.getNextBlock();
+    if(m.getCurrentBlock()!=NULL)std::cout<<"Kobuki is now at block ["<< m.getCurrentBlock()->getTagX() <<","<< m.getCurrentBlock()->getTagY() <<"]."<<std::endl;
+    else std::cout<<"Null block position!"<<std::endl;
+
+    if(b!=NULL)std::cout<<"Next block is ["<< b->getTagX() <<", "<< b->getTagY() <<"]."<<std::endl;
+    else std::cout<<"Exploration over!"<<std::endl;
+
     return 0;
 }
 
